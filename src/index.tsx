4ebf97ac283cfaf28c7b9f7eff80c4b0cff3a5af
Dhruv Gursahani <dhruv.gursahani@outlook.com>
2022-06-23 18:36:17 +0530

    style: :lipstick: sidebar component styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import AboutScreen from './screens/about-screen'
 import MainScreen from './screens/main-screen'
+import Sidebar from './components/sidebar'
 const Drawer = createDrawerNavigator()
 
 const App = () => {
   return (
     <Drawer.Navigator
       initialRouteName="Main"
+      drawerContent={props => <Sidebar {...props} />}
       screenOptions={{
         headerShown: false,
         drawerType: 'back',
